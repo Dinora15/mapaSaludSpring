ced3f4f35240e419a3a62f5d223d62791fd58b9b
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:42:24 +0200

    Update Data.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/entidad/Data.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/entidad/Data.java
@@ -24,7 +24,8 @@ public class Data {
 	 public Data() {
 		 
 	 }
-
+	
+	// Constructor con parámetros
 	public Data(Long id, String indicador_code, String country_code, String year) {
 		super();
 		this.id = id;
@@ -32,7 +33,8 @@ public class Data {
 		this.country_code = country_code;
 		this.year=year;
 	}
-	 
+
+	// Constructor con parámetros (sin id)
 	public Data(String indicador_code, String country_code, String year) {
 		super();
 		
@@ -41,6 +43,7 @@ public class Data {
 		this.year=year;
 	}
 
+	// Métodos getter y setter para id
 	public Long getId() {
 		return id;
 	}
@@ -49,6 +52,7 @@ public class Data {
 		this.id = id;
 	}
 
+	// Métodos getter y setter para indicador_code
 	public String getIndicador_code() {
 		return indicador_code;
 	}
@@ -57,6 +61,7 @@ public class Data {
 		this.indicador_code = indicador_code;
 	}
 
+	 // Métodos getter y setter para country_code
 	public String getCountry_code() {
 		return country_code;
 	}
@@ -64,7 +69,8 @@ public class Data {
 	public void setCountry_code(String country_code) {
 		this.country_code = country_code;
 	}
-	
+
+	 // Métodos getter y setter para year
 	public String getYear(){
 		return year;
 	}
