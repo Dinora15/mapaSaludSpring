b7676fc1442a03974710394d6fae20e0b9a38ba5
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:33:05 +0200

    Update DataControlador.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/controlador/DataControlador.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/controlador/DataControlador.java
@@ -23,10 +23,12 @@ import unedMasterSaludMapaSpring.servicio.DataServicio;
 public class DataControlador {
 	@Autowired
 	private DataServicio servicio;
-		
+
+		// Maneja la solicitud GET para "/datas"
 		@GetMapping({"/datas"})	
 		public String obtenerTodos(@PageableDefault( size=2, page=0)Pageable pageable, Model model){		
-			
+
+			// Recupera una lista paginada de datos desde el servicio
 			Page<Data> page=servicio.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));		
 			
 			
@@ -37,6 +39,7 @@ public class DataControlador {
 			var end=Math.min(currentPage + 5, totalPages);
 			
 			if(totalPages >0)
+			// Crea una lista de números de página para la paginación
 			{
 				List<Integer> pageNumbers = new ArrayList<>();
 				for(int i= start; i<=end; i++) {
@@ -44,9 +47,11 @@ public class DataControlador {
 				}
 				model.addAttribute("pageNumbers", pageNumbers);
 			}
+			// Devuelve la vista "datas"
 			return "datas";
 		}
-		
+
+		// Maneja la solicitud GET para "/datas/nuevo"
 		@GetMapping({"/datas/nuevo"})
 		public String mostrarFormularioDeRegistrarData(Model modelo) {
 			Data data=new Data();
@@ -54,35 +59,41 @@ public class DataControlador {
 			return "crear_data";
 		}
 
+		// Maneja la solicitud POST para "/datas"
 		@PostMapping("/datas")
 		public String guardarData(@ModelAttribute("data")Data data)
 		{
 			servicio.guardarData(data);
 			return "redirect:/datas";
 		}
-		
+
+		// Maneja la solicitud GET para "/datas/editar/{id}"
 		@GetMapping("/datas/editar/{id}")
 		public String mostrarFormularioDeEditar(@PathVariable Long id, Model modelo) {
 			modelo.addAttribute("data", servicio.obtenerDataPorId(id));
 			return "editar_data";
 		}
 
+		// Maneja la solicitud POST para "/datas/{id}"
 		@PostMapping("/datas/{id}")
 		public String actualizarData(@PathVariable Long id,@ModelAttribute("data") Data data, Model modelo ) {
-			
+
+			// Recupera los datos existentes desde el servicio
 			Data dataExistente=servicio.obtenerDataPorId(id);
-			
+
+			 // Actualiza los atributos de los datos
 			dataExistente.setId(id);
-		    dataExistente.setIndicador_code(data.getIndicador_code());
-		    dataExistente.setCountry_code(data.getCountry_code());
-		    dataExistente.setYear(data.getYear());
+			dataExistente.setIndicador_code(data.getIndicador_code());
+			dataExistente.setCountry_code(data.getCountry_code());
+			dataExistente.setYear(data.getYear());
 		    
-		    
-		    servicio.actualizarData(dataExistente);
+		    	// Actualiza los datos utilizando el servicio
+		    	servicio.actualizarData(dataExistente);
 			
-			return "redirect:/datas";
+			return "redirect:/datas";// Redirige a "/datas"
 		}
 
+		// Maneja la solicitud GET para "/datas/{id}"
 		@GetMapping({"/datas/{id}"})
 		public String eliminarData(@PathVariable Long id) {
 			
