c00b0cfa4bb47f9ccbb6d5f53b75d8c584b37f82
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:49:50 +0200

    Update CountryServicioImpl.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/CountryServicioImpl.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/CountryServicioImpl.java
@@ -18,35 +18,39 @@ public class CountryServicioImpl implements CountryServicio{
 	@Autowired
 	private CountryRepositorio repositorio;
 
+	 // Obtiene una lista de todos los países
 	@Override
 	public List<country> listarTodosLosCountries() {		
 		return repositorio.findAll();
 	}
 	
-
+	 // Obtiene una lista paginada de todos los países
 	@Override
-public Page<country> findAll(Pageable pageable) {
+	public Page<country> findAll(Pageable pageable) {
 	
-	return repositorio.findAll( pageable);
-}
+		return repositorio.findAll( pageable);
+	}
 
-
+	// Guarda un país en la base de datos
 	@Override
 	public country guardarCountry(country country) {
 		return repositorio.save(country);
 	}
 
+	// Obtiene un país por su ID
 	@Override
 	public country obtenerCountryPorId(Long Id) {
 		return repositorio.findById(Id).get();
 	}
 
+	// Actualiza un país en la base de datos
 	@Override
 	public country actualizarCountry(country country) {
 		// TODO Auto-generated method stub
 		return repositorio.save(country);
 	}
 
+	// Elimina un país por su ID
 	@Override
 	public void eliminarCountry(Long Id) {
 		repositorio.deleteById(Id);
