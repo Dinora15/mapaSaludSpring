332c625f132e6d65d1a5bc1ec4161348696ce529
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:55:37 +0200

    Update DataServicioImpl.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/DataServicioImpl.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/DataServicioImpl.java
@@ -18,42 +18,40 @@ public class DataServicioImpl implements DataServicio{
 	@Autowired
 	private DataRepositorio repositorio;
 	
-	@Override
-	public List<Data> listarTodosLosDatas() {
-		
-		return repositorio.findAll();
-	}
+// Obtiene una lista de todos los datos
+    @Override
+    public List<Data> listarTodosLosDatas() {
+        return repositorio.findAll();
+    }
 
+    // Obtiene una lista paginada de todos los datos
+    @Override
+    public Page<Data> findAll(Pageable pageable) {
+        return repositorio.findAll(pageable);
+    }
 
-	@Override
-public Page<Data> findAll(Pageable pageable) {
-	
-	return repositorio.findAll( pageable);
-}
-	
-	@Override
-	public Data guardarData(Data data) {
-		
-		return repositorio.save(data);
-	}
+    // Guarda un dato en la base de datos
+    @Override
+    public Data guardarData(Data data) {
+        return repositorio.save(data);
+    }
 
-	@Override
-	public Data obtenerDataPorId(Long Id) {
-		
-		return repositorio.findById(Id).get();
-	}
+    // Obtiene un dato por su ID
+    @Override
+    public Data obtenerDataPorId(Long Id) {
+        return repositorio.findById(Id).get();
+    }
 
-	@Override
-	public Data actualizarData(Data data) {
-		
-		return repositorio.save(data);
-	}
+    // Actualiza un dato en la base de datos
+    @Override
+    public Data actualizarData(Data data) {
+        return repositorio.save(data);
+    }
 
-	@Override
-	public void eliminarData(Long Id) {
-		
-		repositorio.deleteById(Id);
-		
-	}
+    // Elimina un dato por su ID
+    @Override
+    public void eliminarData(Long Id) {
+        repositorio.deleteById(Id);
+    }
 
 }
