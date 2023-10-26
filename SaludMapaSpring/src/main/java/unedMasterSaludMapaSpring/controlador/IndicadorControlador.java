310de9cc9045a6d083a7865cbc9c5018d9703260
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:37:56 +0200

    Update IndicadorControlador.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/controlador/IndicadorControlador.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/controlador/IndicadorControlador.java
@@ -22,10 +22,12 @@ import unedMasterSaludMapaSpring.servicio.IndicadorServicio;
 public class IndicadorControlador {
 	@Autowired
 	private IndicadorServicio servicio;
-		
+
+		// Maneja la solicitud GET para "/indicadores"
 		@GetMapping({"/indicadores"})	
 		public String obtenerTodos(@PageableDefault( size=2, page=0)Pageable pageable, Model model){		
-			
+
+			// Recupera una lista paginada de indicadores desde el servicio
 			Page<Indicador> page=servicio.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));		
 			
 			
@@ -46,7 +48,7 @@ public class IndicadorControlador {
 			 return "indicadores";
 		}
 		
-		
+		// Maneja la solicitud GET para "/indicadores/nuevo"
 		@GetMapping({"/indicadores/nuevo"})
 		public String mostrarFormularioDeRegistrarIndicador(Model modelo) {
 			Indicador indicador=new Indicador();
@@ -54,6 +56,7 @@ public class IndicadorControlador {
 			return "crear_indicador";
 		}
 
+		// Maneja la solicitud POST para "/indicadores"
 		@PostMapping("/indicadores")
 		public String guardarIndicador(@ModelAttribute("indicador")Indicador indicador)
 		{
@@ -61,31 +64,38 @@ public class IndicadorControlador {
 			return "redirect:/indicadores";
 		}
 
+		// Maneja la solicitud GET para "/indicadores/editar/{id}"
 		@GetMapping("/indicadores/editar/{id}")
 		public String mostrarFormularioDeEditar(@PathVariable Long id, Model modelo) {
 			modelo.addAttribute("indicador", servicio.obtenerIndicadorPorId(id));
 			return "editar_indicador";
 		}
 
+		// Maneja la solicitud POST para "/indicadores/{id}"
 		@PostMapping("/indicadores/{id}")
 		public String actualizarIndicador(@PathVariable Long id,@ModelAttribute("indicador") Indicador indicador, Model modelo ) {
-			
+
+			// Recupera el indicador existente desde el servicio
 			Indicador indicadorExistente=servicio.obtenerIndicadorPorId(id);
-			
+
+			// Actualiza los atributos del indicador
 			indicadorExistente.setId(id);
-		    indicadorExistente.setIndicador_name(indicador.getIndicador_name());
-		    indicadorExistente.setIndicador_code(indicador.getIndicador_code());
-		    indicadorExistente.setSource_nota(indicador.getSource_nota());
-		    indicadorExistente.setSource_organization(indicador.getSource_organization());
+		   	indicadorExistente.setIndicador_name(indicador.getIndicador_name());
+		    	indicadorExistente.setIndicador_code(indicador.getIndicador_code());
+		    	indicadorExistente.setSource_nota(indicador.getSource_nota());
+		    	indicadorExistente.setSource_organization(indicador.getSource_organization());
 		    
-		    servicio.actualizarIndicador(indicadorExistente);
+		    	// Actualiza el indicador utilizando el servicio
+			servicio.actualizarIndicador(indicadorExistente);
 			
 			return "redirect:/indicadores";
 		}
 
+		// Maneja la solicitud GET para "/indicadores/{id}"
 		@GetMapping({"/indicadores/{id}"})
 		public String eliminarIndicador(@PathVariable Long id) {
 			
+			// Elimina el indicador utilizando el servicio
 			servicio.eliminarIndicador(id);
 			return "redirect:/indicadores";
 		}
