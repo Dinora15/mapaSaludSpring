e6a471b765e953725c972c8c4609b1a6032ef7f8
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:46:46 +0200

    Update UsuariosRepositorio.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/repositorio/UsuariosRepositorio.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/repositorio/UsuariosRepositorio.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import unedMasterSaludMapaSpring.entidad.Usuarios;
 
 
-
+// Esta interfaz hereda métodos CRUD estándar de JpaRepository para la entidad "usuarios"
 public interface UsuariosRepositorio extends JpaRepository<Usuarios, Long>{
 
 	   Usuarios findByUsername(String username);
