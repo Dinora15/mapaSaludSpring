1f52cc8c21aefa4e7f8432e405fddbca657c08f4
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:48:29 +0200

    Update CountryServicio.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/CountryServicio.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/CountryServicio.java
@@ -9,17 +9,17 @@ import unedMasterSaludMapaSpring.entidad.country;
 
 
 public interface CountryServicio {
-	
+	// Obtiene una lista de todos los países
 	public List<country> listarTodosLosCountries();
-	
+	// Obtiene una lista paginada de todos los países
 	public Page<country> findAll(Pageable pageable);
-	
+	// Guarda un país en la base de datos
 	 public country guardarCountry(country country);
-	 
+	 // Obtiene un país por su ID
 	 public country obtenerCountryPorId(Long Id);
-	    
+	     // Actualiza un país en la base de datos
 	    public country actualizarCountry(country country);
-	    
+	    // Elimina un país por su ID
 	    public void eliminarCountry(Long Id);
 
 }
