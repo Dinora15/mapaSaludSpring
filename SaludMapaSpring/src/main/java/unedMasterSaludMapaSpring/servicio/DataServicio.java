a9c9f9f617af3f304e92c203c46ded3679802fe3
sofiatraba <47029859+sofiatraba@users.noreply.github.com>
2023-10-26 15:51:23 +0200

    Update DataServicio.java

--- SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/DataServicio.java
+++ SaludMapaSpring/src/main/java/unedMasterSaludMapaSpring/servicio/DataServicio.java
@@ -7,16 +7,22 @@ import org.springframework.data.domain.Pageable;
 
 public interface DataServicio {
 	
-	public List<Data> listarTodosLosDatas();
-	
-	public Page<Data> findAll(Pageable pageable);
-	
-	public Data guardarData(Data data);
-	
-	public Data obtenerDataPorId(Long Id);
-    
+    // Obtiene una lista de todos los datos
+    public List<Data> listarTodosLosDatas();
+
+    // Obtiene una lista paginada de todos los datos
+    public Page<Data> findAll(Pageable pageable);
+
+    // Guarda un dato en la base de datos
+    public Data guardarData(Data data);
+
+    // Obtiene un dato por su ID
+    public Data obtenerDataPorId(Long Id);
+
+    // Actualiza un dato en la base de datos
     public Data actualizarData(Data data);
-    
+
+    // Elimina un dato por su ID
     public void eliminarData(Long Id);
 
 }
